c34bc465a8d37cd4a70be9edc71c99a6c838da67
Kevin He <he.changbin@bugtags.com>
2016-08-11 20:09:17 +0800

    improve script

--- app.sh
+++ app.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-usage(){ echo "Usage: $0 [r]" 1>&2; exit 1; }
-
 BUILD_TYPE=$1
 
 if [ -z "${BUILD_TYPE}" ]; then
@@ -10,10 +8,7 @@ if [ -z "${BUILD_TYPE}" ]; then
     BUILD_TYPE="release"
 fi
 
-first=`echo $BUILD_TYPE|cut -c1|tr [a-z] [A-Z]`
-second=`echo $BUILD_TYPE|cut -c2-`
-
-./gradlew -p app clean "assemble$first$second"
+./gradlew -p app clean "assemble${BUILD_TYPE}"
 
 adb install -r "app/build/outputs/apk/app-${BUILD_TYPE}.apk"
 adb shell am start -n com.bugtags.demo/.MainActivity
--- network.sh
+++ network.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-usage(){ echo "Usage: $0 [r]" 1>&2; exit 1; }
-
 BUILD_TYPE=$1
 
 if [ -z "${BUILD_TYPE}" ]; then
@@ -10,10 +8,7 @@ if [ -z "${BUILD_TYPE}" ]; then
     BUILD_TYPE="release"
 fi
 
-first=`echo $BUILD_TYPE|cut -c1|tr [a-z] [A-Z]`
-second=`echo $BUILD_TYPE|cut -c2-`
-
-./gradlew -p network clean "assemble$first$second" > bugtags.log
+./gradlew -p network clean "assemble${BUILD_TYPE}"
 
 adb install -r "network/build/outputs/apk/network-${BUILD_TYPE}.apk"
 adb shell am start -n com.bugtag.networkapp/.MainActivity
